e929aaab74d7e87b7904b67121a1c13408322344
seimiyajun <seimiyajun@gmail.com>
2018-12-16 11:45:24 +0900

    20181216 work in progress.

---
+++ p39_data_definition_for_ui/app.js
@@ -0,0 +1,32 @@
+const items = [
+  {
+    name: '鉛筆',
+    price: 300,
+    quantity: 0,
+  },
+  {
+    name: 'ノート',
+    price: 400,
+    quantity: 0,
+  },
+  {
+    name: '消しゴム',
+    price: 500,
+    quantity: 0,
+  },
+]
+
+var vm = new Vue({
+  el: '#app',
+  data: {
+    items: items
+  }
+})
+
+vm.$watch(function () {
+  return this.items[0].quantity
+}, function(quantity){
+  console.log(quantity)
+})
+
+window.vm = vm
