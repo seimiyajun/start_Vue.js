f030dcf4720b63bc5c0209d24e150013fb16af8b
seimiyajun <seimiyajun@gmail.com>
2019-01-04 21:11:51 +0900

    finish 2.9.3

--- p39_data_definition_for_ui/app.js
+++ p39_data_definition_for_ui/app.js
@@ -7,19 +7,46 @@ const items = [
   {
     name: 'ノート',
     price: 400,
-    quantity: 0,
+    quantity: 1,
   },
   {
     name: '消しゴム',
     price: 500,
-    quantity: 0,
+    quantity: 1,
   },
 ]
 
-let vm = new Vue({
+const vm = new Vue({
   el: '#app',
   data: {
     items: items
+  },
+  filters: {
+    numberWithDelimiter: function (value) {
+      if (!value) {
+        return '0'
+      }
+      return value.toString().replace(/(\d)(?=(\d{3})+$)/g, '$1,')
+    }
+  },
+  computed: {
+    totalPrice: function () {
+      return this.items.reduce(function (sum, item) {
+        return sum + (item.price * item.quantity)
+      }, 0)
+    },
+    totalPriceWithTax: function () {
+      return Math.floor(this.totalPrice * 1.08)
+    },
+    canBuy: function () {
+      // 1000円以上から購入可能にする
+      return this.totalPrice >= 1000
+    },
+    cannotBuyClass: function () {
+      return {
+        cannotBuy: !this.canBuy
+      }
+    }
   }
 });
 
@@ -31,5 +58,23 @@ vm.$watch(function () {
 
 window.vm = vm;
 
+const logged_in_button = new Vue({
+  el: '#b-button',
+  data:{
+    loggedInButton: 'ログイン済のため購入できます',
+    items: items
+  },
+  computed: {
+    totalPrice: function () {
+      return this.items.reduce(function (sum, item) {
+        return sum + (item.price * item.quantity)
+      }, 0)
+    },
+    canBuy: function () {
+      // 1000円以上から購入可能にする
+      return this.totalPrice >= 1000
+    }
+  }
+})
 
 // window.vue = vue;
